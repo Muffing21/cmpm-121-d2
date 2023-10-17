43f8e88203f566545b38231874c2c4fa8a50368b
wichapas <wpichetp@ucsc.edu>
2023-10-16 17:20:35 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,16 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
+const appTitle: HTMLElement = document.createElement("h1");
+appTitle.innerHTML = "Drawing Game";
+
+const canvas: HTMLCanvasElement = document.createElement("canvas");
+canvas.height = 256;
+canvas.width = 256;
+
+app.append(canvas);
 app.append(header);
+app.append(appTitle);
+
+// const ctx = canvas.getContext("2d");
